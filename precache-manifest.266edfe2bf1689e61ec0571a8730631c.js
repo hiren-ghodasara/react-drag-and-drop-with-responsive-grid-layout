9513d52905e4cc5dcef67a4ca7d5fa975a11a979
Hiren patel <hiren.patel@medsymphony.com>
2019-04-26 19:07:50 +0530

    Updates

---
+++ precache-manifest.266edfe2bf1689e61ec0571a8730631c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "deee7c53dbff60554037",
+    "url": "/react-drag-and-drop-with-responsive-grid-layout/static/js/runtime~main.7ec3b3a2.js"
+  },
+  {
+    "revision": "4e3237ba028f34a082fd",
+    "url": "/react-drag-and-drop-with-responsive-grid-layout/static/js/main.b77890ee.chunk.js"
+  },
+  {
+    "revision": "32ee0e8aeaacedc848aa",
+    "url": "/react-drag-and-drop-with-responsive-grid-layout/static/js/2.15a30254.chunk.js"
+  },
+  {
+    "revision": "4e3237ba028f34a082fd",
+    "url": "/react-drag-and-drop-with-responsive-grid-layout/static/css/main.882e93db.chunk.css"
+  },
+  {
+    "revision": "32ee0e8aeaacedc848aa",
+    "url": "/react-drag-and-drop-with-responsive-grid-layout/static/css/2.42ab4234.chunk.css"
+  },
+  {
+    "revision": "0baea1d1f74a8cad1f2f5a9bf880ee0f",
+    "url": "/react-drag-and-drop-with-responsive-grid-layout/index.html"
+  }
+];
