415364800da6e5766013098d783f89fea192443b
Hiren patel <hiren.patel@medsymphony.com>
2019-04-26 20:04:58 +0530

    Add crop images function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { Modal, Spin } from 'antd';
+import ReactCrop from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
 import "./App.css";
 import { initialState } from "./data";
 
+// Drag And Drop Function
 const getListStyle = isDraggingOver => ({
   background: isDraggingOver ? "#563d7b26" : "#fff",
   padding: 8
@@ -12,8 +16,8 @@ const move = (source, destination, droppableSource, droppableDestination) => {
   const sourceClone = Array.from(source);
   const destClone = Array.from(destination);
   const [removed] = sourceClone.splice(droppableSource.index, 1);
-
-  destClone.splice(droppableDestination.index, 0, removed);
+  console.log("destination", destination.length);
+  destClone.splice(destination.length, 0, removed);
 
   const result = {};
   result[droppableSource.droppableId] = sourceClone;
@@ -22,10 +26,21 @@ const move = (source, destination, droppableSource, droppableDestination) => {
   return result;
 };
 
+
 class App extends Component {
   state = {
     itemsCol1: initialState.list1,
-    itemsCol2: initialState.list2
+    itemsCol2: initialState.list2,
+    visible: false,
+    loading: false,
+    src: null,
+    crop: {
+      aspect: 16 / 9,
+      width: 100,
+      height: 100,
+      x: 50,
+      y: 50
+    }
   };
 
   id2List = {
@@ -56,95 +71,266 @@ class App extends Component {
     }
   };
 
+  // Images Crop Function
+  onSelectFile = e => {
+    if (e.target.files && e.target.files.length > 0) {
+      this.uploadFileToCloudinary(e.target.files[0], "droppable1");
+    }
+  };
+  onImageLoaded = (image, crop) => {
+    this.imageRef = image;
+    // this.setState({
+    //   crop: makeAspectCrop(0),
+    //   height: image.height,
+    //   width: image.width
+    // });
+  };
+
+  onCropComplete = crop => {
+    this.makeClientCrop(crop);
+  };
+
+  onCropChange = crop => {
+    this.setState({ crop });
+  };
+  async makeClientCrop(crop) {
+    if (this.imageRef && crop.width && crop.height) {
+      const croppedImageUrlData = await this.getCroppedImg(
+        this.imageRef,
+        crop,
+        "newFile.jpeg"
+      );
+
+      this.setState({ croppedImageUrl: croppedImageUrlData.url, blobImgData: croppedImageUrlData.blobData });
+    }
+  }
+
+  getCroppedImg(image, crop, fileName) {
+    const canvas = document.createElement("canvas");
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(blob => {
+        if (!blob) {
+          //reject(new Error('Canvas is empty'));
+          console.error("Canvas is empty");
+          return;
+        }
+        blob.name = fileName;
+        window.URL.revokeObjectURL(this.fileUrl);
+        this.fileUrl = window.URL.createObjectURL(blob);
+        resolve({ url: this.fileUrl, blobData: blob });
+      }, "image/jpeg");
+    });
+  }
+  //Modal Pops Functions
+  handleOk = (e) => {
+    this.uploadFileToCloudinary(this.state.blobImgData, "droppable2");
+  }
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+  //Upload To cloudinary
+  uploadFileToCloudinary(file, col = "droppable1") {
+    const cloudName = 'dgxpqt1hz';
+    const unsignedUploadPreset = 'ml_default';
+    const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
+    let xhr = new XMLHttpRequest();
+    let fd = new FormData();
+    let reactThis = this;
+    this.setState({ loading: true });
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+    // Update progress (can be used to show progress indicator)
+    xhr.upload.addEventListener("progress", function (e) {
+      console.log(`fileuploadprogress data.loaded: ${e.loaded},data.total: ${e.total}`);
+    });
+
+    xhr.onreadystatechange = function (e) {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // File uploaded successfully
+        var response = JSON.parse(xhr.responseText);
+        if (response.url) {
+          reactThis.state[reactThis.id2List[col]].push({ id: Math.random(), imgUrl: response.url });
+          reactThis.setState({
+            visible: false,
+            loading: false,
+          });
+        }
+      }
+    };
+    fd.append('upload_preset', unsignedUploadPreset);
+    fd.append('tags', 'browser_upload'); // Optional - add tag for image admin in Cloudinary
+    fd.append('file', file);
+    fd.append('folder', 'images');
+    xhr.send(fd);
+  }
+
+  _handleDoubleClickItem(item) {
+    //console.log('I got double-clicked!', item);
+    let reactThis = this;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", item.imgUrl);
+    xhr.responseType = "blob";
+    xhr.onload = function (e) {
+      var urlCreator = window.URL || window.webkitURL;
+      var imageUrl = urlCreator.createObjectURL(this.response);
+      reactThis.setState({ src: imageUrl, visible: true })
+    };
+    xhr.send();
+  }
+
   render() {
+    const { crop, croppedImageUrl, src } = this.state;
     return (
       <div className="App">
         <nav className="navbar navbar-dark bg-dark text-center">
           <span className="navbar-brand mb-0 h1 mx-auto">React Drag And Drop With Responsive Grid Layout (React Beautiful DND)</span>
         </nav>
-        <div className="container-fluid">
-          <DragDropContext onDragEnd={this.onDragEnd}>
-            <div className="row my-3">
-              <Droppable droppableId="droppable1">
-                {(provided, snapshot) => (
-                  <div
-                    className="col-lg-6 col-xs-12 pr-2"
-                    ref={provided.innerRef}
-                  >
-                    <div
-                      className="grid-list-container"
-                      style={getListStyle(snapshot.isDraggingOver)}
-                    >
-                      <ul className="actul-img-left">
-                        {this.state.itemsCol1.map((item, index) => (
-                          <Draggable
-                            key={item.id}
-                            draggableId={item.id}
-                            index={index}
-                          >
-                            {(provided, snapshot) => (
-                              <li
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                              >
-                                <img
-                                  alt="img-alr"
-                                  className="image-item img-zom"
-                                  src={item.imgUrl}
-                                />
-                              </li>
-                            )}
-                          </Draggable>
-                        ))}
-                        {provided.placeholder}
-                      </ul>
-                    </div>
+        <Spin spinning={this.state.loading}>
+          <div className="container-fluid">
+            <DragDropContext onDragEnd={this.onDragEnd}>
+              <div className="row mt-2">
+                <div className="col-lg-12 col-xs-12 mb-2">
+                  <div className="file-up btn btn-outline-primary">
+                    Add New Image
+                    <input type="file" onChange={this.onSelectFile} />
                   </div>
-                )}
-              </Droppable>
-              <Droppable droppableId="droppable2">
-                {(provided, snapshot) => (
-                  <div
-                    className="col-lg-6 col-xs-12 pl-2"
-                    ref={provided.innerRef}
-                  >
+                  {/* <input type="file" onChange={this.onSelectFile} /> */}
+                </div>
+                <Droppable droppableId="droppable1">
+                  {(provided, snapshot) => (
                     <div
-                      className="grid-list-container"
-                      style={getListStyle(snapshot.isDraggingOver)}
+                      className="col-lg-6 col-xs-12 pr-2"
+                      ref={provided.innerRef}
                     >
-                      <ul className="row resize-image no-gutters">
-                        {this.state.itemsCol2.map((item, index) => (
-                          <Draggable
-                            key={item.id}
-                            draggableId={item.id}
-                            index={index}
-                          >
-                            {(provided, snapshot) => (
-                              <li
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                className="col-6 col-md-5 col-lg-4"
-                              >
-                                <img
-                                  alt="img-alr"
-                                  className="image-item"
-                                  src={item.imgUrl}
-                                />
-                              </li>
-                            )}
-                          </Draggable>
-                        ))}
-                        {provided.placeholder}
-                      </ul>
+
+                      <div
+                        className="grid-list-container"
+                        style={getListStyle(snapshot.isDraggingOver)}
+                      >
+                        <ul className="actul-img-left">
+                          {this.state.itemsCol1.map((item, index) => (
+                            <Draggable
+                              key={item.id}
+                              draggableId={item.id}
+                              index={index}
+                            >
+                              {(provided, snapshot) => (
+                                <li
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  onDoubleClick={() => this._handleDoubleClickItem(item)}
+                                >
+                                  <img
+                                    alt="img-alr"
+                                    className="image-item img-zom"
+                                    src={item.imgUrl}
+                                  />
+                                </li>
+                              )}
+                            </Draggable>
+                          ))}
+                          {provided.placeholder}
+                        </ul>
+                      </div>
                     </div>
-                  </div>
+                  )}
+                </Droppable>
+                <Droppable droppableId="droppable2">
+                  {(provided, snapshot) => (
+                    <div
+                      className="col-lg-6 col-xs-12 pl-2"
+                      ref={provided.innerRef}
+                    >
+                      <div
+                        className="grid-list-container"
+                        style={getListStyle(snapshot.isDraggingOver)}
+                      >
+                        <ul className="row resize-image no-gutters">
+                          {this.state.itemsCol2.map((item, index) => (
+                            <Draggable
+                              key={item.id}
+                              draggableId={item.id}
+                              index={index}
+                            >
+                              {(provided, snapshot) => (
+                                <li
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  className="col-6 col-md-5 col-lg-4"
+                                >
+                                  <img
+                                    alt="img-alr"
+                                    className="image-item"
+                                    src={item.imgUrl}
+                                  />
+                                </li>
+                              )}
+                            </Draggable>
+                          ))}
+                          {provided.placeholder}
+                        </ul>
+                      </div>
+                    </div>
+                  )}
+                </Droppable>
+              </div>
+            </DragDropContext>
+          </div>
+        </Spin>
+        <Modal
+          visible={this.state.visible}
+          style={{ top: 17 }}
+          title="Crop Image"
+          onOk={this.handleOk}
+          width={1200}
+          onCancel={this.handleCancel}
+        > <Spin spinning={this.state.loading}>
+            <div className="row">
+
+              <div className="col-lg-6 col-xs-12 crop-ele d-flex justify-content-center align-items-center">
+                {src && (
+                  <ReactCrop
+                    src={src}
+                    crop={crop}
+                    onImageLoaded={this.onImageLoaded}
+                    onComplete={this.onCropComplete}
+                    onChange={this.onCropChange}
+                  />
                 )}
-              </Droppable>
-            </div>
-          </DragDropContext>
-        </div>
+              </div>
+              <div className="col-lg-6 col-xs-12 preview-ele d-flex justify-content-center align-items-center">
+                {croppedImageUrl && (
+                  <img alt="Crop" style={{ maxWidth: "100%", width: "80%" }} src={croppedImageUrl} />
+                )}
+              </div>
+
+            </div></Spin>
+        </Modal>
       </div>
     );
   }
--- src/data.js
+++ src/data.js
@@ -1,20 +1,17 @@
+
 export const initialState = {
     list1: [
-      {id: Math.random(), imgUrl: "https://upload.wikimedia.org/wikipedia/commons/d/de/Windows_live_square.JPG"},
-      {id: Math.random(), imgUrl: "https://upload.wikimedia.org/wikipedia/commons/7/71/Weaved_truncated_square_tiling.png"},
-      {id: Math.random(), imgUrl: "http://5.imimg.com/data5/FC/KN/MY-537032/square-hole-perforated-sheet-250x250.jpg"},
-      {id: Math.random(), imgUrl: "https://pbs.twimg.com/profile_images/846659478120366082/K-kZVvT8.jpg"},
-      {id: Math.random(), imgUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c6/Sierpinski_square.jpg"},
-      {id: Math.random(), imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Square-white.svg/2000px-Square-white.svg.png"},
-      {id: Math.random(), imgUrl: "https://is2-ssl.mzstatic.com/image/thumb/Purple71/v4/6c/31/82/6c3182cd-f718-d550-181f-051f4148a2e4/mzl.qmwzcqcf.png/1200x630bb.jpg"},
-      {id: Math.random(), imgUrl: "http://www.pricolproperty.com/wp-content/uploads/2015/10/Caledon-Square-Dusky-view5626086ccd4d9-1024x905.jpg"},
-      {id: Math.random(), imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQDVW5GsNRdqdEI7liVuh9JLsyKOMgBKZwRcd96A5S_6iFIBc8Tw"},
-      {id: Math.random(), imgUrl: "http://www.bhaktigroup.in/upload/image/project/18/safal_square.jpg"},
-      {id: Math.random(), imgUrl: "https://www.makemytrip.com/travel-guide/media/dg_image/singapore/Parkview-Square-Singapore.jpg"},
-      {id: Math.random(), imgUrl: "http://jmdgroup.in/wp-content/uploads/2016/07/IMG_20160822_180726-515x400.jpg"},
-      {id: Math.random(), imgUrl: "http://e4336fa0c208189c0eee-e6761b7f2db36d2d2df00f6c169a70d2.r66.cf1.rackcdn.com/lps/assets/u/who1299ex-178939-W-Union-Square---Exterior.jpg"},
-      {id: Math.random(), imgUrl: "https://static.wixstatic.com/media/871b06_91f267016e8b4422b10fa7aedf205529.jpg"},
-      {id: Math.random(), imgUrl: "https://i.pinimg.com/originals/75/be/a9/75bea96bc04e2493167be9810ef3bc8c.jpg"},
+      {id: Math.random(), imgUrl: `https://res.cloudinary.com/dgxpqt1hz/image/upload/v1556286429/fixImg/1_10_yt0pdq.jpg`},
+      {id: Math.random(), imgUrl: `https://res.cloudinary.com/dgxpqt1hz/image/upload/v1556286427/fixImg/1_9_m64axr.jpg`},
+      {id: Math.random(), imgUrl: `https://res.cloudinary.com/dgxpqt1hz/image/upload/v1556286427/fixImg/1_12_bqcmav.jpg`},
+      {id: Math.random(), imgUrl: `https://res.cloudinary.com/dgxpqt1hz/image/upload/v1556286427/fixImg/1_13_xvkoly.jpg`},
+      {id: Math.random(), imgUrl: `https://res.cloudinary.com/dgxpqt1hz/image/upload/v1556286427/fixImg/1_11_lamq5j.jpg`},
+      {id: Math.random(), imgUrl: `https://res.cloudinary.com/dgxpqt1hz/image/upload/v1556286427/fixImg/1_6_ecjwnm.jpg`},
+      {id: Math.random(), imgUrl: `https://res.cloudinary.com/dgxpqt1hz/image/upload/v1556286426/fixImg/1_7_lpoao6.jpg`},
+      {id: Math.random(), imgUrl: `https://res.cloudinary.com/dgxpqt1hz/image/upload/v1556286426/fixImg/1_8_ghojgb.jpg`},
+      {id: Math.random(), imgUrl: `https://res.cloudinary.com/dgxpqt1hz/image/upload/v1556286426/fixImg/1_1_mvrxw1.png`},
+      {id: Math.random(), imgUrl: `https://res.cloudinary.com/dgxpqt1hz/image/upload/v1556286426/fixImg/1_5_gvde1y.jpg`},
+      {id: Math.random(), imgUrl: `https://res.cloudinary.com/dgxpqt1hz/image/upload/v1556286426/fixImg/1_1_liocpx.jpg`},
     ],
     list2: [
         
